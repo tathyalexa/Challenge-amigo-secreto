5a0190c5b65ce9d129d4348c35ad65c2d0ed6e9b
Tatielle <tathy_cps-alexa@hotmail.com>
2025-09-21 18:04:22 -0300

    Função para adcionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let participantes = [];
+
+function adicionarAmigo() {
+    let nomeInput = document.getElementById("amigo").value.trim();
+
+    if (nomeInput !== "") {
+        participantes.push(nomeInput);
+        alert(`${nomeInput} foi adicionado à lista!`);
+        document.getElementById("amigo").value = ""; // limpa input
+        console.log("Participantes:", participantes);
+    } else {
+        alert("Digite um nome válido!");
+    }
+}
+
