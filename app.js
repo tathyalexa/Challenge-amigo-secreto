b84c13069e48c47940a1f89f527682f8c6a11576
Tatielle <tathy_cps-alexa@hotmail.com>
2025-09-21 19:39:59 -0300

    Atualizar lista de participantes no html

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 let participantes = [];
 
+// Adicionar participante
 function adicionarAmigo() {
-    let nomeInput = document.getElementById("amigo").value.trim();
+    const nomeInput = document.getElementById("amigo").value.trim();
 
-    if (nomeInput !== "") {
-        participantes.push(nomeInput);
-        alert(`${nomeInput} foi adicionado à lista!`);
-        document.getElementById("amigo").value = ""; // limpa input
-        console.log("Participantes:", participantes);
-    } else {
+    if (nomeInput === "") {
         alert("Digite um nome válido!");
+        return;
     }
+
+    if (participantes.includes(nomeInput)) {
+        alert("Esse participante já foi adicionado!");
+        return;
+    }
+
+    participantes.push(nomeInput);
+    alert(`${nomeInput} foi adicionado à lista!`);
+    document.getElementById("amigo").value = "";
+
+  // Atualiza a lista de participantes no HTML
+    const listaUl = document.getElementById("listaAmigos");
+    const li = document.createElement("li");
+    li.textContent = nomeInput;
+    listaUl.appendChild(li);
+
+    console.log("Participantes:", participantes);
 }
 
